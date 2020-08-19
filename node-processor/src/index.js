bef821fbbe1018e2291f75f57404c50b25b152e2
Huw Angle <Huw.Angle@wales.nhs.uk>
2020-08-19 22:17:07 +0100

    add side menu

--- node-processor/src/index.js
+++ node-processor/src/index.js
@@ -2,20 +2,63 @@ const fs = require('fs')
 const path = require('path')
 const { getHtmlFromMarkdown, applyTemplate, forEachFile } = require('./module')
 
-forEachFile(
-    "./data/user-guides/", 
-    ".md", 
-    function (filePath) {
+processUserGuides()
 
-        const content = getHtmlFromMarkdown(filePath);
-    
-        const page = applyTemplate("./data/api-details/index.html", content)
+async function processUserGuides(){
 
-        const outFile = filePath.replace("user-guides","api-guides").replace(".md", ".html")
-        fs.mkdirSync(path.dirname(outFile), { recursive: true})
-        console.log("writing " + outFile)
+    const navMenus = {}
+    let started = false;
+    let inProgress = 0;
+    forEachFile(
+        "./data/user-guides/", 
+        ".md", 
+        function (filePath) {
+            
+            inProgress++ 
+            started = true
 
-        fs.writeFile(outFile, page.innerHTML, (err) => { console.log( err || "done" )})
+            const { api, documentTitle, documentHref } = processFilePath(filePath)
+
+
+            navMenus[api] = navMenus[api] || []
+            navMenus[api].api = api
+            navMenus[api].push({text: documentTitle, href: documentHref })
+
+            inProgress--
+        }
+    )
+    console.log("building navigation...")
+    while(!started || inProgress){
+        console.log("...still building... ")
+        await new Promise((r) => setTimeout(()=> r(true),100))
     }
-)
+    console.log("...done!")
+
+    forEachFile(
+        "./data/user-guides/", 
+        ".md", 
+        function (filePath) {
+
+            const { api, outFile } = processFilePath(filePath)
+
+            const content = getHtmlFromMarkdown(filePath);
+        
+            const page = applyTemplate("./data/api-details/index.html", content, navMenus[api])
+
+            fs.mkdirSync(path.dirname(outFile), { recursive: true})
+            console.log("writing " + outFile)
+
+            fs.writeFile(outFile, page.innerHTML, (err) => { console.log( err || "done" )})
+        }
+    )
+
+}
+
+function processFilePath(filePath) {
+    const documentHref = filePath.replace(/.*user-guides/, '/api-guides').replace(".md", "/").replace(/\\/g, "/")
+    const documentTitle = path.basename(filePath, ".md")
+    const api = documentHref.split("/")[2]
+    const outFile = filePath.replace("user-guides","api-guides").replace(".md", "/index.html")
+    return { api, documentTitle, documentHref, outFile }
+}
 
